e41de2b1ce3fc8c04a681aa56e46d6e085040496
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-19 21:13:11 -0300

    feat: add PreviousPageBtn

---
+++ src/components/PreviousPageBtn.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PreviousPageBtn(props: { text: string; to: string }) {
+  const { text, to } = props;
+  return (
+    <Link to={to} className="flex py-1">
+      <img
+        height="15px"
+        width="15px"
+        className="invert"
+        src="/src/assets/icons/chevron_left.svg"
+        alt="Voltar para Loja"
+      />
+      {text}
+    </Link>
+  );
+}
+
+export default PreviousPageBtn;
