d54bbb05fdaabd5a5d1a4762f7713d36b8fb5835
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-19 21:17:44 -0300

    wip: Homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react';
+import VehiclesList from '../components/VehiclesList';
+import { useStores } from '../providers/StoresProvider';
+import { observer } from 'mobx-react';
+import useGetVehicles from '../data/use-get-vehicles';
+
+function Homepage() {
+  const { basketStore } = useStores();
+
+  const [page, setPage] = useState<number>(1);
+
+  const { vehicles } = useGetVehicles(page);
+  return (
+    <div className="mx-auto lg:max-w-4xl flex justify-center flex-col">
+      <h1 className="text-center">Homepage</h1>
+      <p>{basketStore.basketItems.length}</p>
+      <VehiclesList vehicles={vehicles} />
+      <div className="my-4 mx-auto flex">
+        <button
+          onClick={() => setPage((prevValue) => prevValue - 1)}
+          disabled={!vehicles?.previous}
+          className="bg-yellow-300 rounded drop-shadow-xl hover:bg-yellow-500 px-4 py-1 brightness-150 font-starwars mx-1 flex disabled:opacity-25"
+        >
+          Anterior
+        </button>
+        <button
+          onClick={() => setPage((prevValue) => prevValue + 1)}
+          disabled={!vehicles?.next}
+          className="bg-yellow-300 rounded drop-shadow-xl hover:bg-yellow-500 px-4 py-1 brightness-150 font-starwars mx-1 flex disabled:opacity-25"
+        >
+          Próxima
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default observer(Homepage);
