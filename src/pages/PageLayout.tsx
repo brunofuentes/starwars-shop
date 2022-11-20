188377607e768ce213d0f96719f5fd45dd3435ed
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-19 21:18:20 -0300

    style: change layout, page width, bg-color

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import Basket from '../components/Basket';
 import Navbar from '../components/Navbar';
 
 function PageLayout() {
   return (
-    <div className="min-h-screen w-full bg-orange-100">
+    <div className="min-h-screen w-full bg-zinc-900 font-mono">
       <Navbar />
-      <div className="relative flex">
-        <Outlet />
-        <Basket />
+      <div className="mx-auto lg:max-w-4xl flex justify-center flex-col">
+        <div className="relative flex">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
