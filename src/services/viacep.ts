137906fa0e6e79678f9e8256a1d180d9ace57c94
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 01:59:33 -0300

    chore: changes on viacep api request

--- src/services/viacep.ts
+++ src/services/viacep.ts
@@ -1,22 +1,22 @@
-import axios from 'axios'
-import { GetCEPResponse } from '../types/cep'
+import axios from 'axios';
+import { GetCEPResponse } from '../types/cep';
 
-const BASE_URL = `https://viacep.com.br/ws`
+const BASE_URL = `https://viacep.com.br/ws`;
 
 const api = {
-	getAddress: async (cep: string | number) => {
-		try {
-			const { data, status } = await axios.get<GetCEPResponse>(`${BASE_URL}/${cep}/json`)
-			return { data, status }
-		} catch (error) {
-			if (axios.isAxiosError(error)) {
-				console.log('error message: ', error.message)
-			} else {
-				console.log('unexpected error: ', error)
-				return 'An unexpected error occurred'
-			}
-		}
-	},
-}
+  getAddress: async (cep: string | number) => {
+    try {
+      const { data } = await axios.get<GetCEPResponse>(`${BASE_URL}/${cep}/json`);
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  },
+};
 
-export default api
+export default api;
