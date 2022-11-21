d9c77799704a2129fb2a0df0d661c49cc64e800e
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 15:47:08 -0300

    add bg-image to PageLayout

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -3,9 +3,9 @@ import Navbar from '../components/Navbar';
 
 function PageLayout() {
   return (
-    <div className="min-h-screen w-full bg-zinc-900 font-mono">
+    <div className="min-h-screen w-full font-mono bg-[url('/src/assets/bg-starwars.png')] bg-contain">
       <Navbar />
-      <div className="mx-auto lg:max-w-4xl flex justify-center flex-col">
+      <div className="mx-auto lg:max-w-4xl flex justify-center flex-col ">
         <Outlet />
       </div>
     </div>
