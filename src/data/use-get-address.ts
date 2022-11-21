59b036717f0758b262e15f1db4ff34e158ff848d
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 16:29:46 -0300

    feat: add isLoading

--- src/data/use-get-address.ts
+++ src/data/use-get-address.ts
@@ -1,21 +1,24 @@
 import { useState, useEffect, useCallback } from 'react';
 import api from '../services/viacep';
 
-const useGetAddress = (cep: string | number | null) => {
-  const [address, setAddress] = useState<any>(null);
+const useGetAddress = (cep: string | number) => {
+  const [address, setAddress] = useState<{}>({});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const refetch = useCallback(() => {
+    setIsLoading(true);
     api
       .getAddress(cep)
-      .then((data) => setAddress(data))
+      .then((data) => setAddress(data?.data))
+      .finally(() => setIsLoading(false))
       .catch((err) => {
         console.error('Error fetching data ', err);
       });
-  }, []);
+  }, [cep]);
 
   useEffect(refetch, [refetch]);
 
-  return { address, refetch };
+  return { address, isLoading, refetch };
 };
 
 export default useGetAddress;
