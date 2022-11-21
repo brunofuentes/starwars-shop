e64604a0aa640fd3d2afe6f469e19dbd3ba29381
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 01:59:10 -0300

    add yup validation schemas

---
+++ src/schemas/index.ts
@@ -0,0 +1,33 @@
+import * as yup from 'yup';
+
+export const addressSchema = yup.object().shape({
+  CEP: yup.string().min(8, 'CEP deve ter 8 dígitos').required('CEP é Obrigatório'),
+  street: yup.string().required('Campo Obrigatório.'),
+  houseNr: yup.string().required('Campo Obrigatório.'),
+  state: yup.string().required('Campo Obrigatório.'),
+  city: yup.string().required('Campo Obrigatório.'),
+  area: yup.string().required('Campo Obrigatório.'),
+});
+
+export const customerSchema = yup.object().shape({
+  firstName: yup.string().min(3, 'O nome deve ter no mínimo 3 caracteres.'),
+  lastName: yup.string().min(3, 'O sobrenome deve ter no mínimo 3 caracteres.'),
+  cpf: yup
+    .string()
+    .trim()
+    .matches(
+      /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/,
+      'CPF/CNPJ Inválido',
+    )
+    .required('Campo Obrigatório.'),
+  email: yup.string().email('Insira um email válido').required('Campo Obrigatório.'),
+});
+
+export const paymentSchema = yup.object().shape({
+  cardNumber: yup
+    .string()
+    .min(8, 'Insira um número de cartão válido')
+    .required('Campo Obrigatório.'),
+  expirationDate: yup.string().required('Campo Obrigatório.'),
+  CVC: yup.string().required('Campo Obrigatório.'),
+});
