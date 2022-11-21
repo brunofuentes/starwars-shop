c56d7a45635906106e2aa5891b69e4229f9121ee
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 15:49:10 -0300

    style: changes on VehicleCard

--- src/components/VehicleCard.tsx
+++ src/components/VehicleCard.tsx
@@ -11,43 +11,38 @@ const VehicleCard = observer((props: { vehicle: Vehicle }) => {
   } = useStores();
 
   return (
-    <div className="sm:w-3/6 md:w-2/6 lg:w-72 w-full bg-zinc-700 m-3 sm:m-1 rounded-md text-gray-200 p-3 shadow-lg flex flex-col">
+    <div className="bg-zinc-800 m-3 sm:m-1 rounded-md text-gray-200 p-3 shadow-lg flex flex-col border-2 border-zinc-700 hover:border-blue-400 opacity-90">
       <div>
-        <p className="text-center py-8 text-2xl font-starwars">{vehicle?.name}</p>
-        <p>Modelo: {vehicle?.model}</p>
+        <p className="text-center py-8 text-2xl font-starwars text-yellow-300 brightness-200">
+          {vehicle?.name}
+        </p>
       </div>
-      <p>Capacidade de carga: {vehicle?.cargo_capacity}</p>
-      <p>Consumíveis: {vehicle?.consumables}</p>
-      <p>Custo em créditos: {vehicle?.cost_in_credits}</p>
-      <p>Tripulação: {vehicle?.crew}</p>
-      <p>
-        Filmes
-        {vehicle?.films.map((film) => (
-          <li key={film}>{film}</li>
-        ))}
+      <p className="border-b border-zinc-700">Modelo: {vehicle?.model}</p>
+      <p className="border-b border-zinc-700">Fabricante: {vehicle?.manufacturer}</p>
+      <p className="border-b border-zinc-700">
+        Velocidade atm máxima: {vehicle?.max_atmosphering_speed}
       </p>
-      <p>Comprimento: {vehicle?.length}</p>
-      <p>Fabricante: {vehicle?.manufacturer}</p>
-      <p>Velocidade atm máxima: {vehicle?.max_atmosphering_speed}</p>
-      <p>Passageiros: {vehicle?.passengers}</p>
-      <p>
-        Pilotos:
-        {vehicle?.pilots.map((pilot) => (
-          <li key={pilot}>{pilot}</li>
-        ))}
-      </p>
-      <p>Passageiros: {vehicle?.passengers}</p>
-      <p>Classe do Veículo: {vehicle?.vehicle_class}</p>
+      <p className="border-b border-zinc-700">Classe: {vehicle?.vehicle_class}</p>
+      <p className="border-b border-zinc-700">Capacidade de carga: {vehicle?.cargo_capacity}</p>
+      <p className="border-b border-zinc-700">Tripulação: {vehicle?.crew}</p>
+      <p className="border-b border-zinc-700">Comprimento: {vehicle?.length}</p>
+      <p className="border-b border-zinc-700">Passageiros: {vehicle?.passengers}</p>
+      {vehicle?.consumables !== 'unknown' && <p>Consumíveis: {vehicle?.consumables}</p>}
+      <p className="border-b border-zinc-700">Custo em créditos: {vehicle?.cost_in_credits}</p>
 
       <div className="mt-auto mx-auto py-4">
         <button
           disabled={vehicle?.cost_in_credits === 'unknown'}
-          className="bg-red-600 rounded drop-shadow-xl hover:bg-red-500 px-4 py-1 brightness-150 disabled:opacity-25"
+          className={`${
+            vehicle?.cost_in_credits === 'unknown'
+              ? 'bg-red-500'
+              : 'bg-green-800 hover:bg-green-600'
+          }  rounded drop-shadow-xl  px-4 py-1 brightness-125 disabled:opacity-25 transition ease-in-out duration-300`}
           onClick={() => {
             addBasketItem(vehicle);
           }}
         >
-          Adicionar ao carrinho
+          {vehicle?.cost_in_credits === 'unknown' ? 'Indisponível' : 'Adicionar ao carrinho'}
         </button>
       </div>
     </div>
