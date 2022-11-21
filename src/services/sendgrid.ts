ced5261c83a6c7df3f10e5887f4a50a87bbff4d3
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-11-21 16:26:37 -0300

    add todo

---
+++ src/services/sendgrid.ts
@@ -0,0 +1,10 @@
+//todo: implementar função de envio de emails com sendgrid
+// tutorial (nextjs): https://www.freecodecamp.org/news/how-to-build-a-working-contact-form-with-sendgrid-and-next-js/
+
+/**
+ * A funcionalidade de enviar emails ao final do processo de compra ainda não foi implementada.
+ * Recomenda-se a implementação a partir do backend, para a devida proteção de dados sigilosos (API-Key).
+ * Desta forma, seria possível os seguintes caminhos:
+ * 1. Utilizar o framework Next.js + React, que possibilita a implementação de funções serverless dentro do framework, sem a necessidade de um backend dedicado.
+ * 2. Criar uma aplicação backend separada do frontend (framework: Express.js) para este serviço e realizar o request a partir do frontend.
+ */
